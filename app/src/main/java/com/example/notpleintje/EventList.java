ee20b73adf408ab87783f331b1235a9aebccbbff
julius <juliuslandsman@hotmail.com>
2022-04-22 01:36:06 +0200

    Initial commit

---
+++ app/src/main/java/com/example/notpleintje/EventList.java
@@ -0,0 +1,14 @@
+package com.example.notpleintje;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EventList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.events_list);
+    }
+}
