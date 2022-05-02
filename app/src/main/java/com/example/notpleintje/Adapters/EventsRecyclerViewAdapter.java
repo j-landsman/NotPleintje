8c03f6ad14a8d6092ecf3427b7cb90bcf2e10b0f
julius <juliuslandsman@hotmail.com>
2022-05-03 00:12:46 +0200

    Added RecyclerView

---
+++ app/src/main/java/com/example/notpleintje/Adapters/EventsRecyclerViewAdapter.java
@@ -0,0 +1,60 @@
+package com.example.notpleintje.Adapters;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.notpleintje.Models.EventModel;
+import com.example.notpleintje.R;
+
+import java.util.ArrayList;
+
+public class EventsRecyclerViewAdapter extends RecyclerView.Adapter<EventsRecyclerViewAdapter.ViewHolder>{
+    ArrayList<EventModel> eventModelArrayList = new ArrayList<>();
+
+    public EventsRecyclerViewAdapter(ArrayList<EventModel> eventModelArrayList) {
+        this.eventModelArrayList = eventModelArrayList;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View cardView = LayoutInflater.from(parent.getContext()).inflate(R.layout.event_card, parent, false);
+        return new ViewHolder(cardView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.day.setText(eventModelArrayList.get(position).getDay());
+        holder.month.setText(eventModelArrayList.get(position).getMonth());
+        holder.title.setText(eventModelArrayList.get(position).getTitle());
+        holder.place.setText(eventModelArrayList.get(position).getPlace());
+        holder.count.setText(eventModelArrayList.get(position).getCount());
+        Glide.with(holder.imageView).load(eventModelArrayList.get(position).getUrl()).into(holder.imageView);
+    }
+
+    @Override
+    public int getItemCount() {
+        return eventModelArrayList.size();
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder{
+        TextView day, month, title, place, count;
+        ImageView imageView;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            day = itemView.findViewById(R.id.day);
+            month = itemView.findViewById(R.id.month);
+            title = itemView.findViewById(R.id.eventTitle);
+            place = itemView.findViewById(R.id.location);
+            count = itemView.findViewById(R.id.count);
+            imageView = itemView.findViewById(R.id.card_image);
+        }
+    }
+}
--- app/src/main/java/com/example/notpleintje/EventList.java
+++ app/src/main/java/com/example/notpleintje/EventList.java
@@ -1,14 +1,29 @@
 package com.example.notpleintje;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
-public class EventList extends AppCompatActivity {
+import com.example.notpleintje.Adapters.EventsRecyclerViewAdapter;
+import com.example.notpleintje.Repository.EventsRepo;
 
+public class EventList extends AppCompatActivity {
+    RecyclerView recyclerView;
+    EventsRecyclerViewAdapter eventsRecyclerViewAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.events_list);
+        recyclerView = findViewById(R.id.recyclerView);
+        initRecyclerView();
+    }
+
+    void initRecyclerView(){
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        eventsRecyclerViewAdapter = new EventsRecyclerViewAdapter(EventsRepo.getEventsRepo().getEventModelList());
+        recyclerView.setLayoutManager(linearLayoutManager);
+        recyclerView.setAdapter(eventsRecyclerViewAdapter);
     }
 }
