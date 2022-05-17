9a7059f75c40a318b2b95dab1405c71fe0bf53dc
julius <juliuslandsman@hotmail.com>
2022-05-17 21:58:55 +0200

    Event count fix

--- app/src/main/java/com/example/notpleintje/EventList.java
+++ app/src/main/java/com/example/notpleintje/EventList.java
@@ -8,20 +8,33 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.notpleintje.Adapters.EventsRecyclerViewAdapter;
+import com.example.notpleintje.Models.EventModel;
 import com.example.notpleintje.Repository.EventsRepo;
+
+import java.util.ArrayList;
 
 public class EventList extends AppCompatActivity {
     RecyclerView recyclerView;
     EventsRecyclerViewAdapter eventsRecyclerViewAdapter;
+    private ArrayList<EventModel> eventModelArrayList;
+    private TextView eventAmtTxt;
     private ImageView backBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.events_list);
+        eventModelArrayList = EventsRepo.getEventsRepo().getEventModelList();
         recyclerView = findViewById(R.id.recyclerView);
         initRecyclerView();
+
+        int eventAmt = eventModelArrayList.size();
+        String eventAmtString = eventAmt + " events in my area";
+        eventAmtTxt = (TextView) findViewById(R.id.event_amt_text);
+        eventAmtTxt.setText(eventAmtString);
 
         backBtn = (ImageView) findViewById(R.id.back_btn);
         backBtn.setOnClickListener(new View.OnClickListener() {
@@ -34,7 +47,7 @@ public class EventList extends AppCompatActivity {
 
     void initRecyclerView(){
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
-        eventsRecyclerViewAdapter = new EventsRecyclerViewAdapter(EventsRepo.getEventsRepo().getEventModelList());
+        eventsRecyclerViewAdapter = new EventsRecyclerViewAdapter(eventModelArrayList);
         recyclerView.setLayoutManager(linearLayoutManager);
         recyclerView.setAdapter(eventsRecyclerViewAdapter);
     }
